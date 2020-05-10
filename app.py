8a374650aac13aa445a09351ab5291d22fbaf26d
Njabulo Mkhonto <jarvis@MacBooks-MacBook-Pro.local>
2020-05-10 18:20:50 +0200

    First flask commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='web/static',
+            template_folder='web/templates')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/register')
+def login():
+    return render_template('register.html')
+
+
+app.run(port=5000, debug=True)
